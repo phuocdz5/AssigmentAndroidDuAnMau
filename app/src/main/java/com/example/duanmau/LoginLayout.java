4d24c22885dcab08a5d78545433143a7e2c10510
phuocdz5 <phuoccntt2004@gmail.com>
2023-12-01 11:30:25 +0700

    git commit -m'11.30'

--- app/src/main/java/com/example/duanmau/LoginLayout.java
+++ app/src/main/java/com/example/duanmau/LoginLayout.java
@@ -21,7 +21,7 @@ import com.example.duanmau.Fragment.ChangePassword;
 public class LoginLayout extends AppCompatActivity {
     private Button btnBack, btnLogin;
     private EditText edUser,edPass;
-    private LinearLayout linearLayout;eloasd
+    private LinearLayout linearLayout;eloasddsadas
     private CheckBox checkBox;
     private SharedPreferences sharedPreferences;
     @Override
