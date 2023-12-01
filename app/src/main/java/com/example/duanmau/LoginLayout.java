1a63eb9fc74b02169ce52d8d6b4327478515024d
phuocdz5 <phuoccntt2004@gmail.com>
2023-12-01 10:19:21 +0700

    git commit -m'phuoc'

--- app/src/main/java/com/example/duanmau/LoginLayout.java
+++ app/src/main/java/com/example/duanmau/LoginLayout.java
@@ -22,7 +22,7 @@ public class LoginLayout extends AppCompatActivity {
     private Button btnBack, btnLogin;
     private EditText edUser,edPass;
     private LinearLayout linearLayout;
-    private CheckBox checkBox,đâsdas;
+    private CheckBox checkBox;dsad
     private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
